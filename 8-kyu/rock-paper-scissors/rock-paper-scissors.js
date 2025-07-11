6f2bd232104e8b6c5614e8041a377c37b7d82165
Reine Mizero <156176469+MizeroR@users.noreply.github.com>
2025-07-11 11:52:57 +0200

    Update solution - CodeHub

--- 8-kyu/rock-paper-scissors/rock-paper-scissors.js
+++ 8-kyu/rock-paper-scissors/rock-paper-scissors.js
@@ -1,26 +1,13 @@
  
 const rps = (p1, p2) => {
-  let message
-  if (p1 === 'rock' && p2 === 'scissors'){
-    message = 'Player 1 won!'
-  } 
-  if (p1 === 'scissors' && p2 === 'paper'){
-    message = 'Player 1 won!'
+  if (p1 === p2) return 'Draw!'
+  
+  const rules = {
+    rock: 'scissors',
+    scissors: 'paper',
+    paper: 'rock'
   }
-  if (p1 === 'paper' && p2 === 'rock'){
-    message = 'Player 1 won!'
-  }
-  if (p1 === 'scissors' && p2 === 'rock'){
-    message = 'Player 2 won!'
-  }
-  if (p1 === 'paper' && p2 === 'scissors'){
-    message = 'Player 2 won!'
-  }
-  if (p1 === 'rock' && p2 === 'paper'){
-    message = 'Player 2 won!'
-  }
-  if (p1 === p2){
-    message = 'Draw!'
-  }
-  return message
+ 
+  return rules.p1 === p2 ? 'Player 1 won!' : 'Player 2 won!'
+​
 };
