c51ce7b7656a774ae81108d9b9fc1243dfb82198
Reine Mizero <156176469+MizeroR@users.noreply.github.com>
2025-07-11 11:45:41 +0200

    Update solution - CodeHub

--- 8-kyu/rock-paper-scissors/rock-paper-scissors.js
+++ 8-kyu/rock-paper-scissors/rock-paper-scissors.js
@@ -22,13 +22,5 @@ const rps = (p1, p2) => {
   if (p1 === p2){
     message = 'Draw!'
   }
-//   p1 = 'rock'
-//   p2 = 'scissors'
-  
-//   p1 = 'scissors'
-//   p2 = 'paper'
-  
-//   p1 = 'paper'
-//   p2 = 'rock'
   return message
 };
