eccc4398a888e74b0aff32101e043966827a517b
Reine Mizero <156176469+MizeroR@users.noreply.github.com>
2025-07-11 12:19:31 +0200

    Update solution - CodeHub

--- 8-kyu/count-of-positives-slash-sum-of-negatives/count-of-positives-slash-sum-of-negatives.js
+++ 8-kyu/count-of-positives-slash-sum-of-negatives/count-of-positives-slash-sum-of-negatives.js
@@ -1,5 +1,7 @@
 function countPositivesSumNegatives(input) {
-  if (!input) return []
+  if (input || input.length === 0) return [] // Refactor it to pass this condition If the input is an empty array or is null, return an empty array.
+​
+​
   let count = 0
   let sum = 0
   for(let i = 0; i<input.length;i++){
