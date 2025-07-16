add239b5fd36a39b71b3557ae2232d378163c63e
Reine Mizero <156176469+MizeroR@users.noreply.github.com>
2025-07-16 10:16:39 +0200

    Update solution - CodeHub

--- 8-kyu/a-needle-in-the-haystack/a-needle-in-the-haystack.js
+++ 8-kyu/a-needle-in-the-haystack/a-needle-in-the-haystack.js
@@ -1,7 +1,7 @@
 function findNeedle(haystack) {
   for(let i = 0; i<haystack.length;i++){
     if(haystack[i] === 'needle'){
-      return `found the needle at position  ${i}`
+      return `found the needle at position ${i}`
     }
   }
 }
