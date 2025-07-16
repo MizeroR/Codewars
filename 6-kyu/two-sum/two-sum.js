a3035e5898783ce8a23ea45f7889611886a2f0c5
Reine Mizero <156176469+MizeroR@users.noreply.github.com>
2025-07-16 10:45:49 +0200

    Update solution - CodeHub

--- 6-kyu/two-sum/two-sum.js
+++ 6-kyu/two-sum/two-sum.js
@@ -1,9 +1,6 @@
- 
 function twoSum(numbers, target) {
   for(let i =0; i<numbers.length;i++){
-    console.log(numbers[i])
     for(let j = i+1; j<numbers.length;j++){
-      console.log(numbers[j])
       if(numbers[i] + numbers[j] === target){
         return [i,j] 
       }
