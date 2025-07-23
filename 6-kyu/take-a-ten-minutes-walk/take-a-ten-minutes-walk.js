9b05edb5f8b798c6801cc60db2ed8af9c714f288
Reine Mizero <156176469+MizeroR@users.noreply.github.com>
2025-07-23 10:44:41 +0200

    Update solution - CodeHub

--- 6-kyu/take-a-ten-minutes-walk/take-a-ten-minutes-walk.js
+++ 6-kyu/take-a-ten-minutes-walk/take-a-ten-minutes-walk.js
@@ -1,17 +1,18 @@
+ 
 function isValidWalk(walk) {
   if(walk.length !== 10) return false
-  let n,s,w,e = 0
+  let n = 0
   for(let i=0; i<walk.length;i++){
     if(walk[i]==='n') n++
   }
-  for(let i=0; i<walk.length;i++){
-    if(walk[i]==='s') s++
-  }
-  for(let i=0; i<walk.length;i++){
-    if(walk[i]==='w') w++
-  }
-  for(let i=0; i<walk.length;i++){
-    if(walk[i]==='e') e++
-  }
-  console.log(n,s,w,e)
+//   for(let i=0; i<walk.length;i++){
+//     if(walk[i]==='s') s++
+//   }
+//   for(let i=0; i<walk.length;i++){
+//     if(walk[i]==='w') w++
+//   }
+//   for(let i=0; i<walk.length;i++){
+//     if(walk[i]==='e') e++
+//   }
+  console.log(n)
 }
