d12b8c82071e58b229094491847b8e6dbe7ad6de
Reine Mizero <156176469+MizeroR@users.noreply.github.com>
2025-07-23 09:50:17 +0200

    Update solution - CodeHub

--- 7-kyu/find-the-divisors/find-the-divisors.js
+++ 7-kyu/find-the-divisors/find-the-divisors.js
@@ -1,9 +1,8 @@
- 
 function divisors(integer) {
   let answer = []
   for (let i = 2; i<integer; i++){
     if(integer%i === 0) answer.push 
   }  
-  return answer.length < 0? `${integer} is prime`: answer
+  return answer.length === 0? `${integer} is prime`: answer
 }
 ​
