852338102691fadf2c80259173afd75272fe1191
Reine Mizero <156176469+MizeroR@users.noreply.github.com>
2025-08-04 10:59:09 +0200

    Add solution - CodeHub

---
+++ 7-kyu/love-vs-friendship/love-vs-friendship.js
@@ -0,0 +1,10 @@
+ 
+function wordsToMarks(string) {
+  let letters = 'abcdefghijklmnopqrstuvwxyz';
+  let sum = 0;
+  for (let i = 0; i < string.length; i++) {
+    let position = letters.indexOf(string[i]) + 1;
+    sum += position;
+  }
+  return sum;
+}
