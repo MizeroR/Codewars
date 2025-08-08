15c002a1c4e5863fa16987f1a06a3844ae83c479
Reine Mizero <156176469+MizeroR@users.noreply.github.com>
2025-08-08 14:56:49 +0200

    Update solution - CodeHub

--- 4-kyu/strings-mix/strings-mix.js
+++ 4-kyu/strings-mix/strings-mix.js
@@ -1,45 +1,26 @@
-function mix(s1, s2) {
-  let countLetters = (s) => {
-    let counts = {}
-    for (let char of s){
-      if(char >='a' && char <= 'z'){
-        counts[char] = (counts[char] || 0) + 1
-      }
+      prefix = '1';
+    } else if (count2 > count1) {
+      prefix = '2';
+    } else {
+      prefix = '=';
     }
-    return counts;
-  }
-  let counts1 = countLetters(s1)
-  let counts2 = countLetters(s2)
-  const letters = new Set([...Object.keys(counts1), ...Object.keys(counts2)]);
-  console.log(letters)
 ​
-  let result = []
-  for(let letter of letters){
-    let count1 = counts1[letter] || 0
-    let count2 = counts2[letter] || 0
-    let maxCount = Math.max(count1,count2)
-    if (maxCount<=1) continue 
-    let prefix;
-    if(count1>count2){
-      prefix ='1'
-    }
-    else if (count2>count1){
-      prefix ='2'
-    }
-    else {
-      prefix = '='
-    }
     result.push(`${prefix}:${letter.repeat(maxCount)}`);
   }
-  
-    result.sort((a, b) => {
-        if (b.length !== a.length) {
-            return b.length - a.length;
-        } else {
-            return a.localeCompare(b);
-        }
-    });
-    return result.join('/')
-  
+​
+  result.sort((a, b) => {
+    if (b.length !== a.length) {
+      return b.length - a.length; 
+    } 
+    const prefixA = a[0];
+    const prefixB = b[0];
+    if (prefixA !== prefixB) {
+      const prefixOrder = {'1': 1, '2': 2, '=': 3};
+      return prefixOrder[prefixA] - prefixOrder[prefixB];
+    }
+    
+    return a.localeCompare(b);
+  });
+​
+  return result.join('/');
 }
-console.log(mix("Are they here", "yes, they are here"))
