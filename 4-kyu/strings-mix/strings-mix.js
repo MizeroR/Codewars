68718d97a059b8a29440c9eb75d51b754affbe43
Reine Mizero <156176469+MizeroR@users.noreply.github.com>
2025-08-08 11:14:59 +0200

    Update solution - CodeHub

--- 4-kyu/strings-mix/strings-mix.js
+++ 4-kyu/strings-mix/strings-mix.js
@@ -11,6 +11,7 @@ function mix(s1, s2) {
   }
   let counts1 = countLetters(s1)
   let counts2 = countLetters(s2)
-  console.log(counts1,counts2)
+  const letters = new Set([...Object.keys(counts1), ...Object.keys(counts2)]);
+  console.log(letters)
   
 }
