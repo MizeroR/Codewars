440ee8a8bfb8fa8d87d33a88f257f46ec9a560de
Reine Mizero <156176469+MizeroR@users.noreply.github.com>
2025-08-08 11:41:13 +0200

    Update solution - CodeHub

--- 4-kyu/strings-mix/strings-mix.js
+++ 4-kyu/strings-mix/strings-mix.js
@@ -12,13 +12,30 @@ function mix(s1, s2) {
   let counts1 = countLetters(s1)
   let counts2 = countLetters(s2)
   const letters = new Set([...Object.keys(counts1), ...Object.keys(counts2)]);
-  
+  console.log(letters)
+​
+  let result = []
   for(let letter of letters){
     let count1 = counts1[letter] || 0
     let count2 = counts2[letter] || 0
-    console.log(count1,count2)
+    let maxCount = Math.max(count1,count2)
+    if (maxCount<=1) continue 
+    let prefix;
+    if(count1>count2){
+      prefix ='1'
+    }
+    else if (count1<count2){
+      prefix ='2'
+    }
+    else {
+      prefix = '='
+    }
+    result.push(`${prefix}:${letter.repeat(maxCount)}`);
+    console.log(result)
+    result.sort((a,b) => {
+      if(a.length!==b.length) return b.length - a.length
+    })
   }
-  console.log(letters)
   
 }
 console.log(mix("Are they here", "yes, they are here"))
