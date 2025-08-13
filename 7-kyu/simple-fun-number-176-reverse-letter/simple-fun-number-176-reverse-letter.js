965216ea4c30458e976438c3c1b7a556076a0b7d
Reine Mizero <156176469+MizeroR@users.noreply.github.com>
2025-08-13 10:28:53 +0200

    Update solution - CodeHub

--- 7-kyu/simple-fun-number-176-reverse-letter/simple-fun-number-176-reverse-letter.js
+++ 7-kyu/simple-fun-number-176-reverse-letter/simple-fun-number-176-reverse-letter.js
@@ -4,5 +4,5 @@ function reverseLetter(str) {
   for (let char of str){
     if((char >= 'a' && char <= 'z') || (char >= 'A' && char <= 'Z')) result.push(char)
   }
-  return result.join('')
+  return result.reverse().join('')
 }
