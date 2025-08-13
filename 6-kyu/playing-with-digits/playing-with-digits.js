f21912c57ef64416cb1045d092568ff9430acb28
Reine Mizero <156176469+MizeroR@users.noreply.github.com>
2025-08-13 11:03:38 +0200

    Update solution - CodeHub

--- 6-kyu/playing-with-digits/playing-with-digits.js
+++ 6-kyu/playing-with-digits/playing-with-digits.js
@@ -1,5 +1,12 @@
+ 
 function digPow(n, p){
-  let num = n.toString().split('')
-  console.log(num)
+  let number = n.toString().split('')
+  let sum = 0
+  for(let i=0; i < number.length; i++){
+    let digit = parseInt(number[i])
+    let power = p+i
+    sum+=digit**power
+  }
+  console.log(sum)
 }
 console.log(digPow(695,2))
