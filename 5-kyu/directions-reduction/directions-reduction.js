568ae344ea387dc88a32c280feb35e2ec066670c
Reine Mizero <156176469+MizeroR@users.noreply.github.com>
2025-08-18 14:10:34 +0200

    Add solution - CodeHub

---
+++ 5-kyu/directions-reduction/directions-reduction.js
@@ -0,0 +1,11 @@
+function dirReduc(arr) {
+    const opposites = {
+        'NORTH': 'SOUTH',
+        'SOUTH': 'NORTH',
+        'EAST': 'WEST',
+        'WEST': 'EAST'
+    };
+    let i = 0;
+    
+    return arr;
+}
